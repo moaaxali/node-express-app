5a2085fce08effbf47cdb2a1babc37993930d933
Maaz Ali <moaaxali@gmail.com>
2021-03-11 18:18:34 +0500

    Add route parameter

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/:name', function(req, res, next) {
+  let name = req.params.name;
+  res.render('index', { title: 'Express', name: name });
+});
+
 module.exports = router;
